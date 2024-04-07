a57b68eb1d51edb70b1a6c384949fbe2dfdfd6a0
yuriallexei <yurialexei@ciencias.unam.mx>
2024-04-07 17:23:25 -0600

    pushi pushi

--- src/modules/votes/services/mintToken.service.ts
+++ src/modules/votes/services/mintToken.service.ts
@@ -3,6 +3,7 @@ import { MintTokenDto } from '../dtos/mintTokens.dto';
 import { ConfigService } from '@nestjs/config';
 import * as chains from 'viem/chains';
 import { http, parseEther, createWalletClient, WalletClient } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
 import * as tokenJson from '../../../../assets/MyToken.json';
 
 @Injectable()
@@ -10,13 +11,18 @@ export class MintTokenService {
   walletClient: WalletClient;
   constructor(private configService: ConfigService) {
     this.walletClient = createWalletClient({
-      chain: chains.sepolia,
-      key: this.configService.get<string>('MINTER_WALLET_PRIVATE_KEY'),
+      account: privateKeyToAccount(
+        this.configService.get<string>(
+          'MINTER_WALLET_PRIVATE_KEY',
+        ) as `0x${string}`,
+      ),
       transport: http(this.configService.get<string>('RPC_ENDPOINT_URL')),
     });
   }
   public async mintToken(mintTokenDto: MintTokenDto): Promise<void> {
     this.walletClient.writeContract({
+      account: this.walletClient.account,
+      chain: chains.sepolia,
       abi: tokenJson.abi,
       address: '0x1231321312' as `0x${string}`,
       functionName: 'mint',
